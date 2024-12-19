01eb56323f380a97e49561a2e81ac783a796d955
dhytalu <nurdita07@gmail.com>
2024-12-19 10:56:23 +0700

    cache

--- app/components/Posts/CardList.tsx
+++ app/components/Posts/CardList.tsx
@@ -1,6 +1,10 @@
-import { Children } from "react";
+import React from 'react';
 
-const CardList = ({ children }: any) => {
+type CardListProps = {
+  children: React.ReactNode;
+};
+
+const CardList = ({ children }: CardListProps) => {
   return <div className="bg-amber-500 w-full p-4">{children}</div>;
 };
 
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from './postPage.module.css'
+// import styles from './postPage.module.css'
 import CardList from '../components/Posts/CardList'
 import ViewUserButton from '../components/Posts/ViewUserButton'
 
@@ -13,14 +13,19 @@ interface Iposts {
 }
 
 const Posts = async() => {
-  const response = await fetch(base_url)
-  const posts:Iposts[] = await response.json()
+  const response = await fetch(base_url, {
+    cache: "no-store", // fetching data jika direload
+    // next: {revalidate: 10} // fetching data setiap 10second
+  })
+  const posts:Iposts[] = await response.json();
+
   return (
     <>
+    <p>{new Date().toLocaleTimeString()}</p>
       <h1 className="text-fuchsia-500">Postingan Page</h1>
       {posts.map((post) =>{
         return (
-          <CardList>
+          <CardList key={post.id}>
             <p>{post.id}</p>
             <p>{post.title}</p>
             <p>{post.body}</p>
